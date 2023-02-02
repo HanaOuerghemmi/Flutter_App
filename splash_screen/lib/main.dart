07a6f748b5e569ea0c11b6cae20133ce8bf4692e
HanaOuerghemmi <5004@holbertonschool.com>
2023-02-02 19:39:56 +0100

    404not found

--- splash_screen/lib/main.dart
+++ splash_screen/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:splash_screen/404.dart';
-import 'package:splash_screen/circle_widgets.dart';
-import 'package:splash_screen/widg.dart';
+//import 'package:splash_screen/circle_widgets.dart';
+//import 'package:splash_screen/widg.dart';
 
 void main() {
   runApp(const MaterialApp(
